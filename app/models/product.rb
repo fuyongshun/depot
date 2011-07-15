fc7657517e7c8011871b2271b40333f7d9f44843
fyslab <fyongshun@gmail.com>
2011-07-15 14:00:16 +0800

    first commit by fys

---
+++ app/models/product.rb
@@ -0,0 +1,9 @@
+class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}    
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with  => %r{\.(gif|png|jpg)$}i,
+    :message => 'must be a URL for gif, jpg or png image.'
+  }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '2ed43cf57ed0b18bc156970a73993b490109a8524cd808fb2c00296578c86dd4c914cc4aa51a211a58f41607cf7d95a45115f624c40a7caf1781fb9b9e6b27c5'
