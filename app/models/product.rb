13a9556d69b86845debd438f9e79fb3d24fd247a
fyslab <fyongshun@gmail.com>
2011-07-18 17:04:13 +0800

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,17 @@
 class Product < ActiveRecord::Base
   
   default_scope :order => 'title'
+  has_many :line_items
   
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "line item present"
+    end
+  end
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}    
   validates :title, :uniqueness => true
