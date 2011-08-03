27f460199aff9e0fada34bf0cba8a07fe29cb886
xiahui@tanei.com <xiahui@tanei.com>
2011-08-03 14:52:57 +0800

    add category

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,9 +4,23 @@ class StoreController < ApplicationController
     if params[:set_locale]
       redirect_to store_path(:locale => params[:set_locale])
     else
-    @products = Product.all
-    @cart = current_cart
-  end
+      if params[:category_id]
+        if (Category.find(params[:category_id])[:parentid] == 0)
+          categorys = Category.find(:all, :conditions => ['parentid = ?', params[:category_id]])
+          cids = params[:category_id];
+          categorys.each do |category|
+            cids += ","+category.id.to_s
+          end
+          @products = Product.find(:all, :conditions => "category_id IN (#{cids})")
+        else
+          @products = Product.find(:all, :conditions => ['category_id = ?', params[:category_id]])
+        end
+      else
+        @products = Product.all
+      end
+      @cart = current_cart
+      @categories = Category.all
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110728085311) do
+ActiveRecord::Schema.define(:version => 20110803062101) do
 
   create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "parentid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -42,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20110728085311) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
   create_table "users", :force => true do |t|
