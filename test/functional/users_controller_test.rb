92d29ff062c54f0526efc923534b23fca9b78905
Yilian <Yilian.ly@gmail.com>
2011-08-04 13:56:10 +0800

    Update Sign Up

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -27,7 +27,7 @@ class UsersControllerTest < ActionController::TestCase
       post :create, :user => @input_attributes
     end
 
-    assert_redirected_to users_path
+    assert_redirected_to admin_url
   end
 
   test "should show user" do
