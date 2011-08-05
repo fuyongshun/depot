280f1092f6431aa0148dc4e359d9afd976917f25
Yilian <Yilian.ly@gmail.com>
2011-08-05 09:26:44 +0800

    ?????????Close gh-7 modify

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Depot::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  # Send email, set the configuration of mailer  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => "smtp.gmail.com",
