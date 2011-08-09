e1596b42c8e83f60cce8de003d63f421ecb8ec81
shaosibiao <shaosibiao@sina.com>
2011-08-09 18:36:02 +0800

    something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     
   	def authorize
   	  unless User.find_by_id(session[:user_id])
-  			redirect_to login_url, :notice => "Please log in"
+  			redirect_to login_url, :notice => I18n.t('.plog')
   		end
   	end
   	
