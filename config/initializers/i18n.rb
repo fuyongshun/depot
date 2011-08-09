41d5a94ec079c39d65cf06a78eabb11b0f9f6714
shaosibiao <shaosibiao@sina.com>
2011-08-09 12:18:57 +0800

    internationalizations

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,5 +3,6 @@ I18n.default_locale = :en
 
 LANGUAGES = [
   ['English',                  'en'],
-  ["Espa&ntilde;ol".html_safe, 'es']
+  ["Espa&ntilde;ol".html_safe, 'es'],
+  ["简体中文".html_safe,'chs']
 ]
